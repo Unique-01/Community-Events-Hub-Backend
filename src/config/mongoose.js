de69a4ca61d07b0e3fc2a2fff993883e198b94c5
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2024-07-09 17:47:05 +0100

    Update Mongodb connection to use env variables

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const MONGODB_SERVER = process.env.MONGODB_SERVER;
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(
-            "mongodb://127.0.0.1:27017/community-events-hub"
-        );
+        await mongoose.connect(MONGODB_SERVER);
         console.log("MongoDB server connected successfully");
     } catch (error) {
         console.log("Unable to connect to the mongoDB server", error);
@@ -12,4 +12,3 @@ const dbConnection = async () => {
 };
 
 dbConnection();
-
